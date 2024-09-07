4f531e00460c47c03e4595e4957288104e15c12f
somnath-db <dbaniksomnath@gmail.com>
2024-09-08 00:30:40 +0530

    small bug fix

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,4 @@
-import React,{useContext, useState,useRef} from 'react'
+import React,{useContext, useState,useRef, useEffect} from 'react'
 import { TodoContext } from '../contexts/todoContext'
 
 function TodoItem({todo}) {
@@ -27,7 +27,7 @@ function TodoItem({todo}) {
           inputRef.current.focus();
         }
       };
-    
+    useEffect(()=>{setTodoContent(todo.todoMsg)},[editable,todo.isCompleted])
   return (
         <div className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
             todo.isCompleted ? "bg-[#dec3bb]" : "bg-[#c9aae0]"
@@ -36,7 +36,7 @@ function TodoItem({todo}) {
             <input ref={inputRef} type='text' value={todoContent} onChange={handleContentChange} readOnly={!editable} className={`border outline-none w-full bg-transparent rounded-lg ${
                     editable ? "border-black/10 px-2" : "border-transparent"
                 } ${todo.isCompleted ? "line-through" : ""}`}/>
-            {!editable &&<button className={`bg-yellow-100 rounded-lg p-1 ${todo.isCompleted?"bg-slate-200 hover:cursor-not-allowed":""}`} onClick={(e)=>{e.stopPropagation();setEditable(!editable);handleFocus();}} disabled={todo.isCompleted}>✏️</button>}
+            {!editable &&<button className={`bg-yellow-100 rounded-lg p-1 ${todo.isCompleted?"bg-slate-50 hover:cursor-not-allowed":""}`} onClick={(e)=>{e.stopPropagation();setEditable(!editable);handleFocus();}} disabled={todo.isCompleted}>✏️</button>}
             {!todo.isCompleted && editable &&<button className="bg-green-700 rounded-lg p-1" onClick={handleUpdate}>📁</button>}
             <button className="bg-red-300 rounded-lg p-1" onClick={handleDelete}>❌</button>
         </div>
